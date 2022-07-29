733e8a7699964d2b377fdacf588f2e07e2b58f46
1ansam <1229921114@qq.com>
2022-07-29 23:02:08 +0800

    添加所有页面

--- app/src/main/java/com/yxf/saoifwiki/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/yxf/saoifwiki/ui/home/HomeViewModel.kt
@@ -7,7 +7,10 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "刀剑神域 关键斗士 Wiki\n作者：秋林(Yansam)\n图片来源：https://saoif.game-info.wiki/"
+        value = "刀剑神域 关键斗士 Wiki" +
+                "\n作者：秋林(Yansam)" +
+                "\n图片来源：https://saoif.game-info.wiki/" +
+                "\n项目地址：https://github.com/1ansam/SAOIF_WIKI_Beta"
 
     }
     val text: LiveData<String> = _text
