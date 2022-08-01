17250304dd11e9782e1021eb6af16a4744142fde
yxf <1anxuf@gmail.com>
2022-08-01 15:21:48 +0800

    change high speed server

--- app/src/main/java/com/yxf/saoifwiki/TopLevel.kt
+++ app/src/main/java/com/yxf/saoifwiki/TopLevel.kt
@@ -4,6 +4,7 @@ package com.yxf.saoifwiki
  *   author:yxf
  *   time:2022/7/29
  */
-const val BASEURL = "http://122.112.139.231:7777"
+const val BASEURL = "http://35.220.132.39:7777"
+//const val BASEURL = "http://122.112.139.231:7777"
 //const val BASEURL = "http://124.70.32.155:7777"
 //const val BASEURL = "http://192.168.2.156:7777"
